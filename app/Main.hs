162328d55f2b5a531c33255c2e14bd3d6bfd8bbb
xpdu <teterevkov.ivan@gmail.com>
2017-11-29 22:30:41 +0000

    upd: tidy up

--- app/Main.hs
+++ app/Main.hs
@@ -3,39 +3,42 @@ module Main where
 import System.IO
 import System.Environment
 
-toInts :: [String] -> [Int]
-toInts = map read
+data Pixel = ColourPixel (Int, Int, Int)
+           | GrayPixel Int
 
-toStrings :: [Int] -> [String]
-toStrings = map show
+instance Show Pixel where
+    show (GrayPixel p) = show p
 
-grayscale :: [Int] -> [Int]
-grayscale [] = []
-grayscale (r:g:b:xs) = [gray] ++ grayscale xs
-    where fr = fromIntegral(r)
-          fg = fromIntegral(g)
-          fb = fromIntegral(b)
-          gray = floor(fr * 0.3 + fg * 0.59 + fb * 0.11)
+data Picture = Picture { width :: Int
+                       , height :: Int
+                       , pixels :: [Pixel]
+                       }
 
-convert :: [String] -> [String]
-convert (x:xs) = header ++ content
-    where header = ["P2"] ++ [show width] ++ [show height]
-          width = il !! 0
-          height = il !! 1
-          il = toInts xs
-          content = [show 255] ++ toStrings img
-          img = grayscale $ drop 3 il
+colourPixels :: [Int] -> [Pixel]
+colourPixels [] = []
+colourPixels (r:g:b:t) = ColourPixel (r, g, b) : colourPixels(t)
+
+colourPicture :: [String] -> Picture
+colourPicture (_:w:h:_:p) = Picture (read w) (read h) (colourPixels (map read p))
+
+toGrayscale :: Pixel -> Pixel
+toGrayscale (ColourPixel (r, g, b)) = GrayPixel ((r * 30 + g * 59 + b * 11) `div` 100)
+
+convertPicture :: (Pixel -> Pixel) -> Picture -> Picture
+convertPicture f (Picture w h pixels) = Picture w h (map f pixels)
+
+serialisePicture :: Picture -> String
+serialisePicture p = unwords $ header ++ content
+    where header = ["P2", show (width p), show (height p), show 255]
+          content = map show (pixels p)
 
 main :: IO ()
 main = do
     args <- getArgs
-    handle <- openFile (head args) ReadMode
-    contents <- hGetContents handle
-    let inp = words contents
-    let outp = convert inp
-
-    handle2 <- openFile "/tmp/dump.pgm" WriteMode
-    hPutStr handle2 (unlines outp)
-
-    hClose handle
-    hClose handle2
+    inp <- openFile (head args) ReadMode
+    outp <- openFile "/tmp/dump.pgm" WriteMode
+    content <- hGetContents inp
+    let grayscalePicture = convertPicture toGrayscale (colourPicture (words content))
+    hPutStr outp (serialisePicture grayscalePicture)
+    hClose outp
+    hClose inp
